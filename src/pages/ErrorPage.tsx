d1478542863cbb939d762064228986da871681f1
eduardo.neto <edu.g.neto06@gmail.com>
2024-10-14 21:47:49 -0300

    alterando estilo de titulo no erropage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,20 +2,22 @@ import { Button, Result } from 'antd';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 const ErrorPage: React.FC = () => {
 
     const handleExit = () => {
         window.location.href = '/dashboard'; 
-      };
-
+    };
 
     return (
         <div>
             <Result
                 status="404"
-                title="Está perdido? clique no botão e volte ao inicio!"
-                extra={<Button onClick={handleExit} type="primary">Clique Aqui</Button>}
+                title={
+                    <span style={{ color: 'white', fontWeight: 'bold' }}>
+                        Está perdido? Clique no botão e volte ao início!
+                    </span>
+                }
+                extra={<Button style={{width:100}} onClick={handleExit} type="dashed">Clique Aqui</Button>}
             />
         </div>
     );
