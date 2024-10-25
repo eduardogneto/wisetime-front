17d54ccc6246b80f2facfb9292f5e9bb3226e903
eduardo.neto <edu.g.neto06@gmail.com>
2024-10-25 10:47:33 -0300

    retirando adicioanr periodos

--- src/pages/DueDateBank/DueDateBank.tsx
+++ src/pages/DueDateBank/DueDateBank.tsx
@@ -1,81 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../components/Header';
 import './style.sass';
-import { EnvironmentOutlined } from '@ant-design/icons';
 import Breadcrumb from '../../components/Breadcrumb/breadcrumb.tsx';
-import { Select, Table, Tag, Modal, Input, DatePicker, message } from 'antd';
+import { Select } from 'antd';
 import DueDateBankTable from './DueDateBankTable.tsx';
 import { TopButtons } from '../../components/TopButtons/TopButtons.tsx';
-import api from '../../connection/api'; 
-import moment, { Moment } from 'moment'; 
 
-const { RangePicker } = DatePicker;
 
 const DueDateBank: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [startDate, setStartDate] = useState<Moment | null>(null);
-  const [endDate, setEndDate] = useState<Moment | null>(null);
-  const [tag, setTag] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  const organizationId = localStorage.getItem('organizationId'); 
-
-  const handleTagChange = (value: string) => {
-    setTag(value);
-  };
-
-  const handleDateChange = (dates: [Moment | null, Moment | null] | null) => {
-    if (dates) {
-      setStartDate(dates[0]);
-      setEndDate(dates[1]);
-    } else {
-      setStartDate(null);
-      setEndDate(null);
-    }
-  };
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    resetFields();
-  };
-
-  const resetFields = () => {
-    setStartDate(null);
-    setEndDate(null);
-    setTag('');
-  };
-
-  const handleSubmit = async () => {
-    if (!startDate || !endDate || !tag) {
-      message.error('Por favor, preencha todos os campos!');
-      return;
-    }
-
-    const payload = {
-      organizationId: organizationId,
-      startDate: startDate.format('YYYY-MM-DD'), 
-      endDate: endDate.format('YYYY-MM-DD'), 
-      tag,
-    };
-
-    setLoading(true);
-
-    try {
-      await api.post('/api/dueDateBank/create', payload);
-      message.success('Período adicionado com sucesso!');
-      setIsModalOpen(false);
-      resetFields();
-    } catch (error) {
-      console.error('Erro ao adicionar o período:', error);
-      message.error('Erro ao adicionar o período.');
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
     <div>
@@ -97,43 +29,11 @@ const DueDateBank: React.FC = () => {
             </div>
             <div className='right-filters'>
               <TopButtons />
-              <div className='button-history'>
-                <button type='button' onClick={handleOpenModal} style={{ marginLeft: 15 }}>
-                  <p>Adicionar</p>
-                </button>
-              </div>
             </div>
           </div>
           <DueDateBankTable />
         </div>
       </div>
-
-      <Modal
-        title="Adicionar Período de Ponto"
-        visible={isModalOpen}
-        onCancel={handleCancel}
-        onOk={handleSubmit}
-        confirmLoading={loading}
-        okText="Salvar"
-        cancelText="Cancelar"
-      >
-        <div className='input-modal'>
-          <h4>Período</h4>
-          <RangePicker format="DD/MM/YYYY" onChange={handleDateChange} />
-        </div>
-        <div className='input-modal'>
-          <h4>Status</h4>
-          <Select
-            placeholder="Selecione o status"
-            onChange={handleTagChange}
-            value={tag}
-            options={[
-              { value: 'COMPLETO', label: 'Completo' },
-              { value: 'ANDAMENTO', label: 'Em andamento' },
-            ]}
-          />
-        </div>
-      </Modal>
     </div>
   );
 };
