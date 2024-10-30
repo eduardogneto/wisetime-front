ec2c3b0e931d884aab7f39f4d44dcc29e50a6b7f
Eduardo G. Neto <89144716+eduardogneto@users.noreply.github.com>
2024-10-30 16:46:22 -0300

    fix

--- src/connection/api.js
+++ src/connection/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: "https://wisetime.site/api",
   headers: {
     'Content-Type': 'application/json',
   },
